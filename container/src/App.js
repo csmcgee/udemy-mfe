56772c1f0ff56b9a72837654b84e55cb7e01c083
Christopher McGee <cmcgee@skylinenet.net>
2022-02-21 17:56:24 -0500

    Added header.

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
   return (
-    <div>
-      <h1>Hello from Container!</h1>
-      <h2>Welcome User</h2>
-      <hr/>
-      <MarketingApp/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp/>
+      </div>
+    </BrowserRouter>
   );
 };
