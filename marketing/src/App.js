3a7b5ffaaeffd383b7c52cd83bff2e798d3b0745
Christopher McGee <cmcgee@skylinenet.net>
2022-02-22 07:11:58 -0500

    css class namespace added.

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 // @todo: put into components directory
 import Landing from './Landing';
 import Pricing from './Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 export default () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/" element={<Landing/>} />
-        </Routes>
-      </BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/" element={<Landing/>} />
+          </Routes>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
   )
 }
