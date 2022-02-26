3e9d34e2b3e44eef28dd9db1841c9d67cd00a4f8
Christopher McGee <cmcgee@skylinenet.net>
2022-02-26 15:29:31 -0500

    typo fix.

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -16,8 +16,8 @@ const prodConfig = {
       name: 'container',
       remotes: {
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-        dashboarad: `auth@${domain}/auth/latest/remoteEntry.js`,
-        dashboarad: `dashboarad@${domain}/dashboarad/latest/remoteEntry.js`,
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+        dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     })
